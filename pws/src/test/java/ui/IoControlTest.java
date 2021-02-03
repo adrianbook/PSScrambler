8c8bb82ab58b91422c87e77941806203fcd2ec51
Staffan <staffan.godman@gmail.com>
2021-02-03 18:26:11 +0100

    Added JUint tests for IoControl methods

---
+++ pws/src/test/java/ui/IoControlTest.java
@@ -0,0 +1,51 @@
+package ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+ 
+
+class IoControlTest {
+	private boolean result;
+
+	IoControl ioControl = new IoControl();
+
+	@Test
+	public void testQuitApplication() {
+		ioControl.setUserPassword("q");
+		if (ioControl.inputQuit()==0) {
+		result = true;
+		}
+		else {
+			result = false;
+		}
+		assertTrue(result);
+	}
+	@Test
+	public void testUserPasswordLength() {
+		ioControl.setUserPassword("qwe");
+		if (ioControl.checkLength()!=-1) {
+			result = false;
+		}
+		else {
+			result = true;
+		}
+		assertTrue(result);
+	}
+	@Test
+	public void testForbiddenCharacters() {
+		String forbiddenChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~";
+		for (char c : forbiddenChars.toCharArray()) {
+			String s = Character.toString(c);
+		ioControl.setUserPassword(s);
+		if (ioControl.checkChars() != 0) {
+			result = true;
+		assertTrue(result);
+		}
+		else {
+			result = false;
+			assertTrue(result);
+		}
+		}
+	}
+}
