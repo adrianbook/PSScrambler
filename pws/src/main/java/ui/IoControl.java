6c50d87efa6358f53691ad77cd1ab6b1a1599d6d
Staffan <staffan.godman@gmail.com>
2021-02-03 16:54:50 +0100

    Replaced temporary Main with real one (which has the user input) and added class IoControl containing input controls.

---
+++ pws/src/main/java/ui/IoControl.java
@@ -0,0 +1,51 @@
+package ui;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class IoControl {
+	private String userPassword;
+	private String checkedUserPassword;
+	private static StringBuilder sb = new StringBuilder();
+	public BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+	public int inputQuit() {
+
+		if (userPassword.trim().toLowerCase().equals("q")) {
+			return 0;
+		}
+		return 1;
+	}
+
+	public int checkLength() {
+		if (userPassword.length() > 3 && userPassword.length() < 11) {
+			return (1);
+		}
+		return (-1);
+	}
+
+	public int checkChars() {
+		sb.setLength(0);
+		for (char ch : userPassword.toCharArray()) {
+			if (String.valueOf(ch).matches("[a-zA-Z_0-9]+") == true) {
+				sb.append(ch);
+				if (sb.length() == userPassword.length()) {
+					checkedUserPassword = sb.toString();
+					System.out.println(checkedUserPassword + " is ok!");
+					System.out.println("New password is:");
+					System.out.println("Björns hemliga formel");
+					return (0);
+				}
+			}
+		}
+		return (-1);
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getCheckedUserPassword() {
+		return checkedUserPassword;
+	}
+}
--- pws/src/main/java/ui/Main.java
+++ pws/src/main/java/ui/Main.java
@@ -6,22 +6,44 @@ import utilities.Cruncher;
 import utilities.Scrambler;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) {
+		IoControl ioControl = new IoControl();
 		Cruncher cruncher = new Cruncher();
-		String s = "Password";
-		
+
+		System.out.println(
+				"Welcome. This application converts a password containing only letters to a more secure password containing 10 letters, digits and special characters");
+		System.out.println("Enter q to quit at any time.");
+		System.out.println("Please enter source password");
+
+		int i = 1;
+		while (i != 0) {
+			i = 1;
+			ioControl.setUserPassword(ioControl.reader.readLine());
+			i = i * ioControl.inputQuit();
+			i = i * ioControl.checkLength();
+			if (i > 0) {
+				i = i * ioControl.checkChars();
+			}
+			if (i < 0) {
+				System.out.println("Source password must contain 4-10 english letters or digits.");
+			}
+		}
+		/*
+		 * Här skall vi anropa metoderna i instanserna av de två andra klasserna för att
+		 * sedan returnera det färdiga lösenordet
+		 */
 		try {
-			String scrambled = Scrambler.scramble(s);			
+			String scrambled = Scrambler.scramble(ioControl.getCheckedUserPassword());
 			String crunched = cruncher.crunch(scrambled);
-			
+
 			System.out.println("Scrambled: " + scrambled);
 			System.out.println("Crunched: " + crunched);
 		}
-		
-		catch (IOException exc) {
-			
-		}
-	}
 
+		catch (IOException exc) {
+		}
+
+		System.out.println("quitting...");
+	}
 }
