a42dc6b8cf78c72bcee90722e1d89172d39c567c
björn munthe <bjomun1011@skola.goteborg.se>
2021-02-03 15:53:30 +0100

    Update Cruncher

--- pws/src/main/java/utilities/Cruncher.java
+++ pws/src/main/java/utilities/Cruncher.java
@@ -5,8 +5,19 @@ public class Cruncher {
 	
 	public String crunch(String s) {
 		builder.append(s);
-		builder.append("Fuck off!");
+		;
+		public void lekaSnallt(Boolean isStygg) {
+			boolean adrianstygg = isStygg;
+			if(adrianstygg = false) {
+				builder.append("Vad söt du är")
+			}
+			else {
+				builder.append("Fuck off!")
+			}
+		}
 		
 		return builder.toString();
 	}
+	
+	
 }
