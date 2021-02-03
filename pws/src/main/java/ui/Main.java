2028b5ce81b4f32781aea3adee8ce622f833c408
Staffan <staffan.godman@gmail.com>
2021-02-03 20:25:59 +0100

    Added condition for running Scrambler and Cruncher classes in Main script, now they dont run if user quits app.

--- pws/src/main/java/ui/Main.java
+++ pws/src/main/java/ui/Main.java
@@ -14,7 +14,7 @@ public class Main {
 		System.out.println(
 				"Welcome. This application converts a password containing only letters to a more secure password containing 10 letters, digits and special characters");
 		System.out.println("Enter q to quit at any time.");
-		System.out.println("Please enter source password");
+		System.out.println("Please enter source password.");
 
 		int i = 1;
 		while (i != 0) {
@@ -33,17 +33,19 @@ public class Main {
 		 * Här skall vi anropa metoderna i instanserna av de två andra klasserna för att
 		 * sedan returnera det färdiga lösenordet
 		 */
+		if (ioControl.inputQuit()!=0){
 		try {
 			String scrambled = Scrambler.scramble(ioControl.getCheckedUserPassword());
 			String crunched = cruncher.crunch(scrambled);
 
-			System.out.println("Scrambled: " + scrambled);
-			System.out.println("Crunched: " + crunched);
+			System.out.println("(Scrambled: " + scrambled + ")");
+			System.out.println("(Crunched: " + crunched + ")");
+			System.out.println(crunched);
 		}
 
 		catch (IOException exc) {
 		}
-
+	}
 		System.out.println("quitting...");
 	}
 }
